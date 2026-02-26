beb59ea9298007957dc47e2c1ae211e1fec37e57
Danila Kaslana <987r122@gmail.com>
2026-02-26 20:24:25 +0300

    adding stars component

---
+++ src/app/shared/components/stars-component/stars-component.ts
@@ -0,0 +1,28 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-stars-component',
+  imports: [],
+  templateUrl: './stars-component.html',
+  styleUrl: './stars-component.css',
+})
+export class StarsComponent {
+  rating = input<number>();
+
+  getStarTypes(rating: number | undefined): string[] {
+    const stars : string[] = [];
+    if (rating == undefined)
+      return stars;
+    
+    for (let i = 0; i < 5; i++) {
+      if (rating >= i + 1) {
+        stars.push('full');
+      } else if (rating >= i + 0.5) {
+        stars.push('half');
+      } else {
+        stars.push('empty');
+      }
+    }
+    return stars;
+  }
+}
