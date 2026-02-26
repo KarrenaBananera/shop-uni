b5d787c19f0cf897d84221c8d5033ac51096b3d7
Danila Kaslana <987r122@gmail.com>
2026-02-26 19:24:56 +0300

    adding json load service

---
+++ src/app/services/product-service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../shared/models/product';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+    private productsUrl = 'products.json';
+
+    constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.productsUrl);
+  }
+}
