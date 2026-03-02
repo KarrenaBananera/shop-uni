59f53a34299176325576fcbabce5ccaa2b29ab56
Danila Kaslana <987r122@gmail.com>
2026-03-02 22:11:39 +0300

    adding header component

---
+++ src/app/shared/components/header-component/header-component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-component',
+  imports: [],
+  templateUrl: './header-component.html',
+  styleUrl: './header-component.css',
+})
+export class HeaderComponent {
+  dropDownOn = false;
+  onDropDownClick()
+  {
+    this.dropDownOn = !this.dropDownOn;
+  }
+}
