220893114ab68fbbb2b3e6a1cedf1740ae004829
Danila Kaslana <987r122@gmail.com>
2026-03-04 09:51:02 +0300

    header small update in visual

--- src/app/shared/components/header-component/header-component.ts
+++ src/app/shared/components/header-component/header-component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-header-component',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header-component.html',
   styleUrl: './header-component.css',
 })
 export class HeaderComponent {
   dropDownOn = false;
-  onDropDownClick()
-  {
+
+  onDropDownClick() {
     this.dropDownOn = !this.dropDownOn;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: UIEvent) {
+    const width = (event.target as Window).innerWidth;
+    if (width >= 760) {
+      this.dropDownOn = false;
+    }
   }
 }
