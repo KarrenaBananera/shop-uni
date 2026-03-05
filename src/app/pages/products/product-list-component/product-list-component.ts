392bc5f5cc09e3134c1a5990618eaefab461a41f
Danila Kaslana <987r122@gmail.com>
2026-03-05 18:15:17 +0300

    moving products filtering to service from component

--- src/app/pages/products/product-list-component/product-list-component.ts
+++ src/app/pages/products/product-list-component/product-list-component.ts
@@ -12,43 +12,7 @@ import { ProductFilterService } from '../../../services/product-filter-service';
   styleUrl: './product-list-component.css',
 })
 export class ProductListComponent {
-  products! : Signal<Product[]>
-  productService = inject(ProductService);
   filterService = inject(ProductFilterService);
-
-  ngOnInit(): void {
-    this.products = this.productService.products;
-  }
-
-
-     filteredProducts = computed(() => {
-    const allProducts = this.products();
-    const { ratingFilter, minPrice, maxPrice } = this.filterService.filter;
-    const { sortBy } = this.filterService.sort;
-
-    let filtered = allProducts.filter(product => {
-      if (ratingFilter() === '5plus' && product.rating < 5) return false;
-      if (ratingFilter() === '4plus' && product.rating < 4) return false;
-      if (ratingFilter() === '3plus' && product.rating < 3) return false;
-      if (minPrice() !== null && product.price < minPrice()!) return false;
-      if (maxPrice() !== null && product.price > maxPrice()!) return false;
-      return true;
-    });
-
-    const sort = sortBy();
-    switch (sort) {
-      case 'name':
-        filtered.sort((a, b) => a.name.localeCompare(b.name));
-        break;
-      case 'priceDesc':
-        filtered.sort((a, b) => b.price - a.price);
-        break;
-      case 'priceAsc':
-        filtered.sort((a, b) => a.price - b.price);
-        break;
-    }
-    return filtered;
-  });
 
   addToCartEvent = output<Product>();
   productClickEvent = output<Product>();
--- src/app/services/product-filter-service.ts
+++ src/app/services/product-filter-service.ts
@@ -1,12 +1,46 @@
-import { Injectable } from '@angular/core';
-import { FilterSettings, SortSettings } from './filter-settings';
+import { computed, inject, Injectable, Signal } from '@angular/core';
+import { FilterSettings, SortSettings } from './model/filter-settings';
+import { ProductService } from './product-service';
+import { Product } from '../shared/models/product';
 
 
 @Injectable({
-  providedIn: 'root' 
+  providedIn: 'root'
 })
 export class ProductFilterService {
+  private productService = inject(ProductService);
+  products = this.productService.products;
   public filter = new FilterSettings();
   public sort = new SortSettings();
+  filteredCount = computed(() => this.filteredProducts().length);
+
+  filteredProducts = computed(() => {
+    const allProducts = this.products();
+    const { ratingFilter, minPrice, maxPrice } = this.filter;
+    const { sortBy } = this.sort;
+
+    let filtered = allProducts.filter(product => {
+      if (ratingFilter() === '5plus' && product.rating < 5) return false;
+      if (ratingFilter() === '4plus' && product.rating < 4) return false;
+      if (ratingFilter() === '3plus' && product.rating < 3) return false;
+      if (minPrice() !== null && product.price < minPrice()!) return false;
+      if (maxPrice() !== null && product.price > maxPrice()!) return false;
+      return true;
+    });
+
+    const sort = sortBy();
+    switch (sort) {
+      case 'name':
+        filtered.sort((a, b) => a.name.localeCompare(b.name));
+        break;
+      case 'priceDesc':
+        filtered.sort((a, b) => b.price - a.price);
+        break;
+      case 'priceAsc':
+        filtered.sort((a, b) => a.price - b.price);
+        break;
+    }
+    return filtered;
+  });
 
 }
