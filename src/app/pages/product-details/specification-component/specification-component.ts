d74f65413dd698841b8fc575881a706fe41109b4
Danila Kaslana <987r122@gmail.com>
2026-03-08 18:32:55 +0300

    adding specifcation component

---
+++ src/app/pages/product-details/specification-component/specification-component.ts
@@ -0,0 +1,18 @@
+import { KeyValuePipe } from '@angular/common';
+import { Component, input, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-specification-component',
+  imports: [KeyValuePipe],
+  templateUrl: './specification-component.html',
+  styleUrl: './specification-component.css',
+})
+export class SpecificationComponent {
+  fields = input.required<Record<string,string>>();
+
+  expanded = signal(false);
+
+  protected toggle(): void {
+    this.expanded.update(prev => !prev);
+  }
+}
