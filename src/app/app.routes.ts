16e249201e49e0ebb6eb69c15b0d3fc3e04722c2
Danila Kaslana <987r122@gmail.com>
2026-03-08 18:35:14 +0300

    adding routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProductsPageComponent } from './pages/products/products-page-component/products-page-component';
+import { ProductDetailsPage } from './pages/product-details/product-details-page/product-details-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: ProductsPageComponent},
+    { path: 'products/:id', component: ProductDetailsPage },
+    { path: '**', redirectTo: '' },
+];
