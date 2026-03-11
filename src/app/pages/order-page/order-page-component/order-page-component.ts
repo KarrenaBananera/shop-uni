11d7e29ba580c9be2894944bf98f334e8a2c7f66
Danila Kaslana <987r122@gmail.com>
2026-03-11 03:21:11 +0300

    adding order page

--- src/app/pages/order-page/order-page-component/order-page-component.ts
+++ src/app/pages/order-page/order-page-component/order-page-component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ProductImageComponent } from "../../../shared/components/product-image-component/product-image-component";
 import { ProductListComponent } from "../../products/product-list-component/product-list-component";
 import { orderListComponent } from "../products-list-component/products-list-component";
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CartService } from '../../../services/cart-service';
 
 @Component({
   selector: 'app-order-page-component',
-  imports: [orderListComponent],
+  imports: [orderListComponent, RouterLink, FormsModule],
   templateUrl: './order-page-component.html',
   styleUrl: './order-page-component.css',
 })
 export class OrderPageComponent {
+  promoCode: string = ''; 
+  cart = inject(CartService);
+  protected isPromoApplied = signal(false);
 
+  onApplyClick()
+  {
+    if (this.promoCode === "BUILD10")
+    {
+      this.cart.discountPercent.set(0.1);
+      this.isPromoApplied.set(true);
+    }
+  }
 }
